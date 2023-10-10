37bf276a57da2b5fb371fb4e7dc29704ca52da5a
camilobeta11 <camilobeta11@hotmail.com>
2023-10-10 09:24:38 -0500

    create persistent authentication

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,21 +1,29 @@
-import { Injectable } from '@angular/core'
-;
+import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-
 import { ILoginForm } from 'src/app/interfaces/interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
 
-  constructor() {}
+  constructor() {
+    const storedAuthState = localStorage.getItem('authState');
+    if (storedAuthState) {
+      const isAuth = JSON.parse(storedAuthState);
+      this.isAuthenticatedSubject.next(isAuth);
+    }
+  }
 
   login(login: ILoginForm): Observable<boolean> {
-    if (login.username === 'usertest' && login.password === '123456' && login.country == 'Brazil') {
+    if (
+      login.username === 'usertest' &&
+      login.password === '123456' &&
+      login.country == 'Brazil'
+    ) {
       this.isAuthenticatedSubject.next(true);
+      localStorage.setItem('authState', JSON.stringify(true));
       return new Observable<boolean>((observer) => {
         setTimeout(() => {
           observer.next(true);
@@ -24,6 +32,7 @@ export class AuthService {
       });
     } else {
       this.isAuthenticatedSubject.next(false);
+      localStorage.setItem('authState', JSON.stringify(false));
       return new Observable<boolean>((observer) => {
         setTimeout(() => {
           observer.next(false);
@@ -39,5 +48,6 @@ export class AuthService {
 
   logout(): void {
     this.isAuthenticatedSubject.next(false);
+    localStorage.removeItem('authState');
   }
 }
