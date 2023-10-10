58e34b18e054f2ed8d8165610d4c8b8ddc19122f
camilobeta11 <camilobeta11@hotmail.com>
2023-10-09 23:58:59 -0500

    navctrl in guard auth

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate } from '@angular/router';
 import { Observable, map } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { NavController, ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private navCtrl: NavController
+    ) {}
 
   canActivate(): Observable<boolean> | Promise<boolean> | boolean {
     return this.authService.isAuthenticated().pipe(map((authenticated: any) => {
       if (authenticated) {
         return true;
       } else {
-        this.router.navigate(['/login']);
+        this.navCtrl.navigateRoot('/login')
         return false;
       }
     }));
